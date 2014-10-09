590aff9ea575408559c9f37d232ea61237a1cbba
chrisnatali <chris.natali@gmail.com>
2014-10-09 15:21:52 -0400

    added script that can be used to build conda pkg and upload to binstar

---
+++ Sequencer/build_upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to build sequencer and upload to binstar
+# Should be run on all architectures
+
+# so that conda doesn't exit after running
+conda config --set always_yes yes --set changeps1 no
+
+echo "Building Sequencer"
+rm -rf $HOME/mr/miniconda/conda-bld/linux-64/sequencer-*.bz2
+conda build Sequencer
+
+echo "Uploading to Binstar"
+binstar upload $HOME/miniconda/conda-bld/linux-64/sequencer-*.bz2
+
