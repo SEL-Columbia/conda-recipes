4fbfe4c9d47dd658273f9d7a05b570746f812e5d
chrisnatali <chris.natali@gmail.com>
2016-10-06 12:46:19 -0400

    update builds for infra, mr, networker and sequencer

--- infrastructure-planning/build.sh
+++ infrastructure-planning/build.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-cat << EOF > setup.py
-from os.path import abspath, dirname, join
-from setuptools import find_packages, setup
-
-
-FOLDER = dirname(abspath(__file__))
-DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
-    'README.rst', 'CHANGES.rst'])
-setup(
-    name='infrastructure-planning',
-    version='2.0.9',
-    description='Infrastructure planning tools',
-    long_description=DESCRIPTION,
-    classifiers=[
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
-    ],
-    author='Roy Hyunjin Han',
-    author_email='rhh@crosscompute.com',
-    url='https://crosscompute.com/docs',
-    keywords='crosscompute',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False
-)
-EOF
+# Bundle invisibleroads-macros straight from gh/master
+# NOTE:  This will install some dependencies via pip
+git clone https://github.com/invisibleroads/invisibleroads-macros
+pushd invisibleroads-macros
+${PYTHON} setup.py install --prefix "${PREFIX}"
+popd
 
 $PYTHON setup.py install 
 
